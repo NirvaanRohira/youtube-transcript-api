137f78d9614af03128890700f925f41243901e04
Nirvaan Rohira <nirvaan.rohira@gmail.com>
2025-06-18 23:57:45 +0530

    done again

--- app.py
+++ app.py
@@ -12,12 +12,15 @@ def transcript():
     url = request.args.get("video", "")
     video_id = ""
 
-    if "watch?v=" in url:
-        video_id = url.split("watch?v=")[-1]
+    # Handle different YouTube URL formats
+    if "youtube.com/watch?v=" in url:
+        video_id = url.split("v=")[1].split("&")[0]
     elif "youtu.be/" in url:
-        video_id = url.split("youtu.be/")[-1]
+        video_id = url.split("youtu.be/")[1].split("?")[0]
+    elif "youtube.com/live/" in url:
+        video_id = url.split("youtube.com/live/")[1].split("?")[0]
     else:
-        return jsonify({"error": "Invalid YouTube URL"}), 400
+        return jsonify({"error": f"Could not extract video ID from URL: {url}"}), 400
 
     try:
         data = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
